9f4ef985ffcd47c6eacc057d2b035d896181e240
pu9052473 <pu9052473@gmail.com>
2023-12-29 16:43:00 +0530

    remove nodemodules

--- test/__tests__/first.js
+++
@@ -1,5 +0,0 @@
-describe("First test suite" , () => {
-    test("First case" , () => {
-        expect(true).toBe(true);        // in expect we give the value , toBe we said that what value should be return , if both match then test "PASS" else "FAIL"
-    })
-})
