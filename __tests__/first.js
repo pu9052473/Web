6b7c7e9f6b27cc13411c8468232f79aa8fe507c0
pu9052473 <pu9052473@gmail.com>
2023-12-29 15:43:45 +0530

    first commit

--- __tests__/first.js
+++ __tests__/first.js
@@ -1,5 +1,5 @@
 describe("First test suite" , () => {
     test("First case" , () => {
-        expect(false).toBe(true);        // in expect we give the value , toBe we said that what value should be return , if both match then test "PASS" else "FAIL"
+        expect(true).toBe(true);        // in expect we give the value , toBe we said that what value should be return , if both match then test "PASS" else "FAIL"
     })
 })
