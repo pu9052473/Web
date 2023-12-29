80ccda036572ccc614ff1d8bd2ff256257a5ba5e
pu9052473 <pu9052473@gmail.com>
2023-12-29 21:56:06 +0530

    wd201 : milestone of L4 : test the all function in todo.js

--- todo-cli/__tests__/first.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-describe("First test suite", () => {
-  test("First case", () => {
-    expect(true).toBe(true); // in expect we give the value , toBe we said that what value should be return , if both match then test "PASS" else "FAIL"
-  });
-});
--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /*
 const todoList = require('../todo');  // import the todo.js file 
@@ -25,33 +26,74 @@ describe("Todolist test suite" , () => {
 })
 */
 
+/*eslint-disable*/
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Todolist test suite", () => {
+  const item = todoList(); // this taken all the function from the todoList() , like all,add,etc.
   beforeAll(() => {
     // this beforeAll exucute the code which we given inside before the all given code
-    add({
+    item.add({
       title: "Test todo",
-      completed: false,
+      completed: false, // we add this data , so it's length is increse , become (1)
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
   });
 
   test("Should add new todo", () => {
-    const todoItemsCount = all.length; // this give all.length=(1) to the variable
-    add({
+    const todoItemsCount = item.all.length; // this give all.length=(1) to the variable
+    item.add({
       title: "Test todo",
-      completed: false,
+      completed: false, // we add this data , so it's length is increse , become (1+1 = 2)
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
-    expect(all.length).toBe(todoItemsCount + 1); // this give value and expect ( todoItemsCount + 1) = (1+1)
+    expect(item.all.length).toBe(todoItemsCount + 1); // this give value and expect ( todoItemsCount + 1) = (1+1)
   });
 
   test("Should mark as todo complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
+    expect(item.all[0].completed).toBe(false); // before it was "false" , so it pass the test
+    markAsComplete(0); // after call this it done the work , it converted from "false" to "true"
+    expect(item.all[0].completed).toBe(true); // after it is become "true" , so it pass the test
+  });
+
+  let Today = new Date();
+  // const today = formattedDate(Today);
+  const yesterday = new Date(Today); // call the Today as new Date
+  yesterday.setDate(Today.getDate() - 1); // after that we cut one from the today's date , so it become the day before , mean yesterday
+  const tomorrow = new Date(Today); // call the Today as new Date
+  tomorrow.setDate(Today.getDate() + 1); // after that we add one from the today's date , so it become the day after , mean tomorrow
+
+  test("check date is always yesterday", async () => {
+    await item.add({
+      dueDate: Today.getDate() - 1, // we check is that's date is  yesterday or not
+    });
+
+    const checkYesterdayDate = await item.overdue(); // call overdue and store in new variable
+    console.log(checkYesterdayDate); // print the value which is changed
+    expect(checkYesterdayDate.length).toBe(1); // value is changed so it add , and length become (1)
+  });
+
+  test("to check today's date", async () => {
+    await item.add({
+      dueDate: Today, // we check this date is today's date or not
+    });
+
+    const checkTodayDate = await item.dueToday(); // call dueToday and store in new variable
+    console.log(checkTodayDate); // print the value , in this value is not changed
+    expect(checkTodayDate.length).toBe(0); // value is not changed , so it's length remain (0)
+  });
+
+  test("check date is always tomorrow", async () => {
+    await item.add({
+      dueDate: Today.getDate() + 1, // we check this date is tomorrow's date or not
+    });
+
+    const checkTomorrowDate = await item.dueLater(); // call dueLater and store in new variable
+    console.log(checkTomorrowDate); // print the value which is changed
+    expect(checkTomorrowDate.length).toBe(1); // value is changed so it add , and length become (1)
   });
 });
+
+/* eslint-enable no-unused-vars */
